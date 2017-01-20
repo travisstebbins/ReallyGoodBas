c6ee8b251986ccd7ad68fbcfd3c2ffc4ed4bbf33
Travis Stebbins <stebbins.travis@gmail.com>
2017-01-20 22:46:32 +0100

    Fix camera movement and remove friction
    Camera only moves when player approaches edge of screen
    Remove friction so player can’t stick to walls

--- ReallyGoodBas/Assets/Scripts/CameraController.cs
+++ ReallyGoodBas/Assets/Scripts/CameraController.cs
@@ -4,13 +4,45 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
-	private GameObject player;
+	// public variables
+	public float scrollDistanceXProportion = 0.7f;
+	public float scrollDistanceYProportion = 0.7f;
+	public float defaultZ = -10f;
 
+	// private variables
+	private GameObject player;
+	private Vector3 startingPos;
+	private float scrollDistanceX;
+	private float scrollDistanceY;
+
+	void Awake () {
+		Time.timeScale = 1;
+	}
+		
 	void Start () {
 		player = GameObject.FindGameObjectWithTag ("Player");
+		startingPos = transform.position;
+		scrollDistanceX = scrollDistanceXProportion * (16.0f/9.0f) * this.GetComponent<Camera>().orthographicSize;
+		scrollDistanceY = scrollDistanceYProportion * this.GetComponent<Camera>().orthographicSize;
+		transform.position = new Vector3 (player.transform.position.x, player.transform.position.y, defaultZ);
 	}
 
 	void Update () {
-		gameObject.transform.position = new Vector3 (player.gameObject.transform.position.x, player.gameObject.transform.position.y, -10);
+		if (player.transform.position.x > transform.position.x + (this.GetComponent<Camera>().orthographicSize * (16.0/9.0) - scrollDistanceX)) {
+			transform.position = new Vector3 (player.transform.position.x - (float)(this.GetComponent<Camera> ().orthographicSize * (16.0 / 9.0) - scrollDistanceX), transform.position.y, defaultZ);
+		}
+		else if (player.transform.position.x < transform.position.x - (this.GetComponent<Camera>().orthographicSize * (16.0/9.0)) + scrollDistanceX) {
+			transform.position = new Vector3 (player.transform.position.x + (float)(this.GetComponent<Camera> ().orthographicSize * (16.0 / 9.0) - scrollDistanceX), transform.position.y, defaultZ);
+		}
+		if (player.transform.position.y > transform.position.y + (this.GetComponent<Camera>().orthographicSize - scrollDistanceY)) {
+			transform.position = new Vector3 (transform.position.x, player.transform.position.y - (float)(this.GetComponent<Camera>().orthographicSize - scrollDistanceY), defaultZ);
+		}
+		else if (player.transform.position.y < transform.position.y - (this.GetComponent<Camera>().orthographicSize - scrollDistanceY)) {
+			transform.position = new Vector3 (transform.position.x, player.transform.position.y + (float)(this.GetComponent<Camera>().orthographicSize - scrollDistanceY), defaultZ);
+		}
+	}
+
+	public void Reset() {
+		transform.position = startingPos;
 	}
 }
