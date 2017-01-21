446a9b2685177a9177c1dd14dde3761ca672c5bd
Travis Stebbins <stebbins.travis@gmail.com>
2017-01-21 16:41:05 +0100

    Introduce basic enemies

---
+++ ReallyGoodBas/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+	// public variables
+	public float movementSpeed = 7f;
+
+	// components
+	Rigidbody2D rb;
+
+	// private variables
+	public GameObject player;
+
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate () {
+		rb.velocity = Vector2.ClampMagnitude( new Vector2 (player.transform.position.x - transform.position.x, player.transform.position.y - transform.position.y), movementSpeed);
+	}
+}
